2bb0e3ce5b871f005989a1daa5250ad7f6a7dddb
MattD82 <mattdevor@gmail.com>
2019-04-18 21:06:33 -0600

    update app style and content

--- app_alt.py
+++ app_alt.py
@@ -8,27 +8,53 @@ jerry_chat_bot = JerryChatBot()
 
 jerry_chat_bot_convos = []
 
-
 @app.route('/', methods=['POST', 'GET'])
 def home():
+    
     if request.method == 'POST':
-        if 'sentence' not in request.form:
-            flash('No sentence post')
-            redirect(request.url)
-        elif request.form['sentence'] == '':
-            flash('No sentence')
-            redirect(request.url)
-        else:
+        print(request.method)
+        print(request.form)
+
+        # if 'sentence' not in request.form:
+        #     flash('No sentence post')
+        #     redirect(request.url)
+
+        # elif request.form['sentence'] == '':
+        #     flash('No sentence')
+        #     redirect(request.url)
+        # if request.form['submit_button'] == 'Do Something':
+        #     print("TEST")
+        #     jerry_chat_bot_convos.clear()
+        # elif request.form['submit_button'] == 'Do Something Else':
+        #     print("TEST2")
+
+        if request.form.get('Clear') == 'Clear':
+            print("DELETE")
+            jerry_chat_bot_convos.clear()
+            #exit()
+
+            return render_template('home.html', conversations=jerry_chat_bot_convos)
+
+        if 'sentence' in request.form:
             sent = request.form['sentence']
             jerry_chat_bot_convos.append('You: ' + sent)
             reply = jerry_chat_bot.reply(sent)
             jerry_chat_bot_convos.append('Jerry: ' + reply)
-    return render_template('home.html', conversations=jerry_chat_bot_convos)  #[-4:]
+
+            return render_template('home.html', conversations=jerry_chat_bot_convos)
+
+ 
+    elif request.method == 'GET':
+        return render_template('home.html')
+    #     
+
+    # else:
+    #     return render_template('home.html')
 
 
 def main():
     jerry_chat_bot.test_run(False)
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True, use_reloader=False)
 
 if __name__ == '__main__':
     main()
